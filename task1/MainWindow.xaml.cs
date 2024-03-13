690d2c8c9f9caa5774f3186aa1ad8c41b88d0fb8
leaha <leaha@DESKTOP-ONNEAOG>
2024-03-13 10:08:40 +0300

    Add project files.

---
+++ task1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private bool drag;
+        private Point startPt;
+
+        private RhombusViewModel viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            viewModel = (RhombusViewModel) DataContext;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point pointClicked = e.GetPosition(canvas);
+            viewModel.AddPoint(pointClicked);
+        }
+
+       
+    }
+}
---
+++ task1/RhombusViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace task1
+{
+    public partial class RhombusViewModel : ObservableObject
+    {
+       
+       
+        private bool isDrawing = false;
+        
+
+
+        [ObservableProperty] private string cordinates = "";
+        [ObservableProperty] private float scale = 1;
+        [ObservableProperty] private int angle = 45;
+        [ObservableProperty] private double diagonal1 = 100;
+        [ObservableProperty] private double diagonal2 = 150;
+        [ObservableProperty] 
+        private Point startPoint;
+        [ObservableProperty] private PointCollection rPoint = new PointCollection();
+        [ObservableProperty] private PointCollection points = new PointCollection();
+
+
+
+
+
+        public RhombusViewModel()
+        {
+            rPoint.Add(startPoint);
+        }
+
+
+
+        public void AddPoint(Point point)
+        {
+            startPoint = point;
+            isDrawing = true;
+            OnPropertyChanged(nameof(RPoint));
+        }
+
+        
+       
+        private List<Point> CalculateRhombusPoints(Point centerPoint, double diagonal1, double diagonal2, double angle)
+        {
+            List<Point> rhombusPoints = new List<Point>();
+
+            double halfDiagonal1 = diagonal1 / 2;
+            double halfDiagonal2 = diagonal2 / 2;
+
+            for (int i = 0; i < 4; i++)
+            {
+                double angleInRadians = (angle + i * 90) * Math.PI / 180;
+                double x = centerPoint.X + Math.Cos(angleInRadians) * (i % 2 == 0 ? halfDiagonal1 : halfDiagonal2);
+                double y = centerPoint.Y + Math.Sin(angleInRadians) * (i % 2 == 0 ? halfDiagonal1 : halfDiagonal2);
+                rhombusPoints.Add(new Point(x, y));
+
+
+            }
+            return rhombusPoints;
+        }
+    
+
+
+   
+        
+        [RelayCommand]
+        public void DrawRhombus()
+        {
+           
+            
+
+
+                Points = new PointCollection(CalculateRhombusPoints(startPoint, 100, 150, 30));
+                isDrawing = false;
+             
+            
+        }
+    }
+}
