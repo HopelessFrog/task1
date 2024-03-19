ac3aa47c01a846b0d0eef66861093eb782f240b8
TalentlessC1own <91304666+TalentlessC1own@users.noreply.github.com>
2024-03-19 18:37:18 +0300

    7887

--- task1/RhombusViewModel.cs
+++ task1/RhombusViewModel.cs
@@ -7,25 +7,42 @@ namespace task1;
 
 public partial class RhombusViewModel : ObservableObject
 {
-    [ObservableProperty] private int angle = 45;
+    RhombusViewModel()
+    {
+        Points = new PointCollection(new List<Point>()
+            { new Point(0, 0), new Point(0, 0), new Point(0, 0), new Point(0, 0) });
+    }
+    private int angle = 45;
 
 
     [ObservableProperty] private string cordinates = "";
     [ObservableProperty] private double diagonal1 = 100;
     [ObservableProperty] private double diagonal2 = 150;
-
-
-    private bool isDrawing;
     [ObservableProperty] private PointCollection points = new();
     [ObservableProperty] private float scale = 1;
 
     [ObservableProperty] private Point startPoint;
 
+    public int Angle
+    {
+        get => angle;
+        set
+        {
+            if (value != angle)
+            {
+                angle = value;
+                OnPropertyChanged();
+                DrawRhombus();
+            }
+        }
+    }
+
 
     public void AddPoint(Point point)
     {
         StartPoint = point;
-        isDrawing = true;
+        DrawRhombus();
+        Cordinates = $"X:{StartPoint.X} Y:{StartPoint.Y}";
     }
 
 
@@ -51,7 +68,6 @@ public partial class RhombusViewModel : ObservableObject
     [RelayCommand]
     public void DrawRhombus()
     {
-        Points = new PointCollection(CalculateRhombusPoints(startPoint, 100, 150, 30));
-        isDrawing = false;
+        Points = new PointCollection(CalculateRhombusPoints(startPoint, Diagonal1, Diagonal2, Angle));
     }
 }
